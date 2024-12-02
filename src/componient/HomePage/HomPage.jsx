c07390fd9b3736831c1b76946808988954afef53
Phung The Ngoc <phungngoccoc@gmail.com>
2024-12-02 11:23:10 +0700

    add section collection

--- src/componient/HomePage/HomPage.jsx
+++ src/componient/HomePage/HomPage.jsx
@@ -3,7 +3,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../Style/HomePage.scss"
-import Category from "../Header/Section/Category";
+import Category from "./Section/Category";
 
 const HomePage = () => {
     const settings = {
---
+++ src/componient/HomePage/Section/Collection.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import "../../Style/Collection.scss"
+
+const Collection = () => {
+    return (
+        <div>
+            hello collection
+        </div>
+    );
+};
+
+export default Collection;
